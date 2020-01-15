338ee53d780cb4bb2de0980895cf2923e8ab5f01
baljnyam <baljnyam@gmail.com>
2020-01-15 16:39:44 +1100

    5.done

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -71,6 +71,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
+# Rest framework pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
